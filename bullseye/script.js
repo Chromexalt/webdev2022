5cfac4ea9bacaf6ecb987b36045ac6a99915aa88
Chromexalt <97337250+Chromexalt@users.noreply.github.com>
2022-01-29 20:23:56 -0800

    Create script.js

---
+++ bullseye/script.js
@@ -0,0 +1,13 @@
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  let person = prompt("Please enter your name", "Harry Potter");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+  }
+}
+</script>
