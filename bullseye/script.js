e455835a6c5a9092eaad39c73192bdf9d8ca44af
Chromexalt <97337250+Chromexalt@users.noreply.github.com>
2022-01-30 04:56:00 -0800

    Update script.js

--- bullseye/script.js
+++ bullseye/script.js
@@ -8,21 +8,28 @@ var el = document.getElementById('blackboard');
 el.innerHTML = GetTableContent(operator, table);
 
 function GetValueFromUser(valueType) {
-  var greetingMessage = 'Hello. How are you? ';
-  }
+  var greetingMessage = 'Hello. How are you?';
+  if (valueType == "operator") {
     greetingMessage += "Enter addition or multiplication";
   }
   else
   {
-    greetingMessage += "Enter a table you would like from 0-10.";
+    greetingMessage += "Enter table";
   }
   return prompt(greetingMessage);
 }
 
-function GetTableContent(table) {
+function GetTableContent(operator, table) {
   var i = 1;                 // Set counter to 1
   var msg = '';              // Message  
-// Do multiplication
+  if (operator === 'addition') {
+    // Do addition
+    while (i < 11) {
+      msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+      i++;
+    }
+  } else {
+    // Do multiplication
     while (i < 11) {
       msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
       i++;
