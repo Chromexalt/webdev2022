a0cea2337741b0cf7636017f692ecc07e24076b4
Chromexalt <97337250+Chromexalt@users.noreply.github.com>
2022-01-29 21:24:17 -0800

    Update script.js

--- bullseye/script.js
+++ bullseye/script.js
@@ -1,13 +1,22 @@
-<button onclick="myFunction()">Try it</button>
+var table = 3;             // Unit of table
+var operator = 'addition'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '';              // Message
 
-<p id="demo"></p>
-
-<script>
-function myFunction() {
-  let person = prompt("Please enter your name", "Harry Potter");
-  if (person != null) {
-    document.getElementById("demo").innerHTML =
-    "Hello " + person + "! How are you today?";
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
   }
 }
-</script>
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
