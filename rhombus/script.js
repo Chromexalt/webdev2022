0aa0cc3e70bc2ed80c0302c0e709582c8c4133a3
Chromexalt <97337250+Chromexalt@users.noreply.github.com>
2022-02-06 01:50:35 -0800

    Update script.js

--- rhombus/script.js
+++ rhombus/script.js
@@ -51,7 +51,7 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
         rLine +="<p>";
 
         //Fill in the left side of every row with spaces or blanks
-        for(x=1; x<=pHeight-i; x++){
+        for(x=1; x<pHeight-i; x++){
         //rLine +="-";
         rLine +="<span class= 'space'>" + pSymbol +"</span>";
         }
