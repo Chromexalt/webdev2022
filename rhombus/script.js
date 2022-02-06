798eccc9110751e8c67a8a3f43fc6f3c81797d63
Chromexalt <97337250+Chromexalt@users.noreply.github.com>
2022-02-05 23:45:04 -0800

    Update script.js

--- rhombus/script.js
+++ rhombus/script.js
@@ -7,7 +7,7 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
     upRight(pHeight, pColorEven, pColorOdd, pSymbol);
     downRight(pHeight, pColorEven, pColorOdd, pSymbol);
-    downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+    
     }
     
     function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -40,42 +40,6 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
         
         document.getElementById("upLeft").innerHTML = rLine;
         }
-
-
-     function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-        var rLine ="";
-        for (i=pHeight;i > 0;i--){
-        rLine +="<p>";
-
-        //Fill in the left side of every row with spaces or blanks
-        for(x=-1; x<=pHeight-i; x++){
-            rLine +="-";
-        
-        }
-
-        //Create each line on the Rhombus
-        for(j=-1;j<i;j++){
-            
-        //Is the position even or odd so we change the color
-        if (j%2)
-        //even
-         rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-        else
-        //odd
-        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-            
-        }
-        rLine +="</p>";
-        // console.log(rLine);
-            
-        }
-            
-        document.getElementById("downLeft").innerHTML = rLine;
-        }
-
-
-
-
 
     function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine ="";
@@ -124,3 +88,4 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     
     document.getElementById("downRight").innerHTML = rLine;
     }
+
