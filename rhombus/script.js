6094ebe9e2c489c61f8bc285dfc794eaf88c745f
Chromexalt <97337250+Chromexalt@users.noreply.github.com>
2022-02-06 01:16:51 -0800

    Update script.js

--- rhombus/script.js
+++ rhombus/script.js
@@ -45,35 +45,35 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 
 //*******************************DOWN LEFT*******************************
 
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-    var rLine ="";
-    for (i=pHeight;i <= 0;i--){
-    rLine +="<p>";
+    function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+        var rLine ="";
+        for (i=pHeight;i <= 0;i--){
+        rLine +="<p>";
 
-    //Fill in the left side of every row with spaces or blanks
-    for(x=1; x<=pHeight-i; x++){
-    //rLine +="-";
-    rLine +="<span class= 'space'>" + pSymbol +"</span>";
-    }
+        //Fill in the left side of every row with spaces or blanks
+        for(x=1; x<=pHeight-i; x++){
+        //rLine +="-";
+        rLine +="<span class= 'space'>" + pSymbol +"</span>";
+        }
 
-    //Create each line on the Rhombus
-    for(j=0;j<i;j++){
-    //Is the position even or odd so we change the color
-    if (j%2)
-    //even
-    rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-    else
-    //odd
-    rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+        //Create each line on the Rhombus
+        for(j=0;j<i;j++){
+        //Is the position even or odd so we change the color
+        if (j%2)
+        //even
+        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+        else
+        //odd
+        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
 
-    }
-    rLine +="</p>";
-    // console.log(rLine);
+        }
+        rLine +="</p>";
+        // console.log(rLine);
 
-    }
+        }
 
-    document.getElementById("downLeft").innerHTML = rLine;
-    }
+        document.getElementById("downLeft").innerHTML = rLine;
+        }
 
 //*******************************UP RIGHT*******************************
 
