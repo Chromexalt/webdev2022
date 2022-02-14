7a5d5fb21e9be49493ca62697abeeb0b37a4db1f
Chromexalt <97337250+Chromexalt@users.noreply.github.com>
2022-02-13 23:05:35 -0800

    Update script.js

--- bullseye/script.js
+++ bullseye/script.js
@@ -8,8 +8,16 @@ var el = document.getElementById('blackboard');
 el.innerHTML = GetTableContent(operator, table);
 
 function GetValueFromUser(valueType) {
-  var operator = window.prompt("Enter a number from 1-10: ");
+  var websiteQuery = 'Multiplication Table says: ';
   if (valueType == "operator") {
+    websiteQuery += "website operational, hit Enter ";
+  }
+  else
+  {
+    websiteQuery += "Enter a number from 0-10 to generate table";
+  }
+  return prompt(websiteQuery);
+}
 
 function GetTableContent(operator, table) {
   var i = 1;                
